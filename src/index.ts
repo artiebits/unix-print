58f46691058c5384d23cd5736f2d4cb9f1f1847a
Pavel Ivanov <ppivanov98@gmail.com>
2023-02-17 23:45:32 +0000

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as print } from "./print/print";
+export { isPrintComplete } from "./utils/parse-response";
 export { default as getDefaultPrinter } from "./get-default-printer/get-default-printer";
 export { default as getPrinters } from "./get-printers/get-printers";
