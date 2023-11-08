e2a03c92e67d305301664574b226e710a9b5e827
Jimmy Nyström <jimmynystrom85@gmail.com>
2023-11-08 10:19:03 +0100

    Added a bit of documentation to the env vars

--- src/utils/exec-async.ts
+++ src/utils/exec-async.ts
@@ -6,6 +6,10 @@ import { exec } from "child_process";
 export default function execAsync(cmd: string): Promise<ExecResponse> {
     return new Promise((resolve, reject) => {
         exec(cmd, {
+            // The output from lp and lpstat is parsed assuming the language is English.
+            // LANG=C sets the language and the SOFTWARE variable is necessary
+            // on MacOS due to a detail in Apple's CUPS implementation
+            // (see https://unix.stackexchange.com/a/33836)
             env: {
                 SOFTWARE: "",
                 LANG: "C"
