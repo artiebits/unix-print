c8965ce4c524ce5d74872c726e87a0c0577ab878
jimnys-8 <jimmynystrom85@gmail.com>
2023-11-24 08:41:35 +0100

    fix: parsing for non-english systems, part 2 (#26)
    * Removed null type from ExecResponse
    (as far as I can tell, exec always returns strings)
    
    * Always use the language env vars in execAsync
    (since the output is always assumed to be in English)
    
    * Removed redundant env vars from cmd input to execAsync
    
    * Added a bit of documentation to the env vars

--- src/utils/exec-async.ts
+++ src/utils/exec-async.ts
@@ -1,7 +1,25 @@
 "use strict";
 
-const { exec } = require("child_process");
-const util = require("util");
-const execAsync = util.promisify(exec);
+import { ExecResponse } from "../types";
+import { exec } from "child_process";
 
-export default execAsync;
+export default function execAsync(cmd: string): Promise<ExecResponse> {
+    return new Promise((resolve, reject) => {
+        exec(cmd, {
+            // The output from lp and lpstat is parsed assuming the language is English.
+            // LANG=C sets the language and the SOFTWARE variable is necessary
+            // on MacOS due to a detail in Apple's CUPS implementation
+            // (see https://unix.stackexchange.com/a/33836)
+            env: {
+                SOFTWARE: "",
+                LANG: "C"
+            }
+        }, (err, stdout, stderr) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({stdout, stderr});
+            }
+        });
+    });
+}
